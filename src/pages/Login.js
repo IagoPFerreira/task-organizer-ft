c1bbbe1b366fb67c57c29071de441eaa19c38e10
iago <iago.pferreiravr@gmail.com>
2022-03-12 18:03:33 -0300

    feat(pages): Login
    Adicionando logica de validar as informacoes dos inputs da pagina de login
    
    Valida se as informacoes passadas nos inputs estão dentro dos parametros esperados

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Login() {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [validEmail, setValidEmail] = useState(false);
+  const [validPassword, setValidPassword] = useState(false);
+  const [disable, setDisable] = useState(true);
+
+  function handleChanges(event, setState, setValid) {
+    const { value } = event.target;
+    setState(value);
+    if (event.target.checkValidity() === true) setValid(true);
+  }
+
   return (
     <section>
       <label htmlFor="login-email-input">
@@ -11,6 +23,9 @@ function Login() {
           placeholder="exemplo@exemplo.com"
           id="login-email-input"
           data-testid="login-email-input"
+          value={ email }
+          onChange={ (e) => handleChanges(e, setEmail, setValidEmail) }
+          pattern="/\S+@\S+\.\S+/"
         />
       </label>
       <label htmlFor="login-password-input">
@@ -20,9 +35,16 @@ function Login() {
           placeholder="min 6 dígitos"
           id="login-password-input"
           data-testid="login-password-input"
+          value={ password }
+          onChange={ (e) => handleChanges(e, setPassword, setValidPassword) }
+          pattern=".{6,}"
         />
       </label>
-      <button type="button" disabled data-testid="login-button-input">
+      <button
+        type="button"
+        disabled={ disable }
+        data-testid="login-button-input"
+      >
         Entrar
       </button>
       <Link to="/register" data-testid="create-account">
