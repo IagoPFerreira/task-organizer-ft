0bce79b80e32487a7303fc8a8b3660715c1364a4
iago <iago.pferreiravr@gmail.com>
2022-03-12 17:36:19 -0300

    feat(pages): Login
    Adicionando elementos basicos da pagina de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-function Login(props) {
+function Login() {
   return (
     <section>
-      
+      <label htmlFor="login-email-input">
+        Email
+        <input
+          type="email"
+          placeholder="exemplo@exemplo.com"
+          id="login-email-input"
+          data-testid="login-email-input"
+        />
+      </label>
+      <label htmlFor="login-password-input">
+        Senha
+        <input
+          type="password"
+          placeholder="min 6 dígitos"
+          id="login-password-input"
+          data-testid="login-password-input"
+        />
+      </label>
+      <button type="button" disabled data-testid="login-button-input">
+        Entrar
+      </button>
+      <Link to="/register" data-testid="create-account">
+        Ainda não tem uma conta? Registre-se aqui
+      </Link>
     </section>
   );
 }
