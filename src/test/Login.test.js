4377e2cc1b074fe71e9f77a3ab59bc94f8642bd1
iago <iago.pferreiravr@gmail.com>
2022-03-12 11:43:01 -0300

    feat(tests): /login
    Implementando a base para os testes na pagina de login

--- src/test/Login.test.js
+++ src/test/Login.test.js
@@ -4,7 +4,5 @@ import App from '../App';
 import renderWithRouter from './renderWithRouter';
 
 describe('Testando a página de login', () => {
-  it('verificando se os componentes são renderizados de corretamente', async () => {
-    const { history } = renderWithRouter(<App />);
-  });
+  
 });
