43d12956fcb2fc10f330e4007fa00c7b33e8774e
iago <iago.pferreiravr@gmail.com>
2022-03-14 16:21:19 -0300

    feat(pages): Login
    Passando como props o data-testid da frase de erro para o componente Forms

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Forms from '../components/Forms';
+import testIds from '../test/dataTestIds';
 
 function Login() {
   return (
@@ -11,6 +12,7 @@ function Login() {
         erroString="Email ou senha incorretos"
         endpoint="http://localhost:8080/login"
         page="tasks"
+        testids={ testIds.incorretEmailOrPassword }
       />
       <Link to="/register" data-testid="create-account">
         Ainda não tem uma conta? Registre-se aqui
