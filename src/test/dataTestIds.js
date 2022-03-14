a8c9a0e47618b75409c41c7367575ce93b4198a7
iago <iago.pferreiravr@gmail.com>
2022-03-14 17:00:44 -0300

    feat(tests): Test Ids
    Adicionando novos data-testids

--- src/test/dataTestIds.js
+++ src/test/dataTestIds.js
@@ -4,6 +4,10 @@ const testIds = {
   loginSubmitButton: 'login-submit-button',
   createAccount: 'create-account',
   incorretEmailOrPassword: 'incorrect-email-or-password',
+  registerNameInput: 'register-name-input',
+  registerEmailInput: 'register-email-input',
+  registerPasswordInput: 'register-password-input',
+  registerSubmitButton: 'register-submit-button'
 };
 
 export default testIds;
