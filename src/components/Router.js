f231929a051c41ff816ea33ad3d2c230d9a3d6e1
iago <iago.pferreiravr@gmail.com>
2022-03-14 11:52:31 -0300

    feat(pages): Rotas
    Adicionando a rota '/register' que renderizara a pagina de registro

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Redirect, Switch } from 'react-router-dom';
 import Login from '../pages/Login';
+import Register from '../pages/Register';
 
 function Router() {
   return (
     <Switch>
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/" render={() => <Redirect to="/login" />} />
+      <Route exact path="/login" component={ Login } />
+      <Route exact path="/register" component={ Register } />
+      <Route exact path="/" render={ () => <Redirect to="/login" /> } />
     </Switch>
   );
 }
