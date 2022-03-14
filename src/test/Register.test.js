5881117e3d6bbb45089efc153ef5b89e6b06f884
iago <iago.pferreiravr@gmail.com>
2022-03-14 16:59:20 -0300

    feat(tests): /register
    Implementando a base para os testes na pagina de registro

---
+++ src/test/Register.test.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+import userEvent from '@testing-library/user-event';
+import testIds from './dataTestIds';
+
+describe('Testando a página de registro', () => {});
